ce1c1395f69230aaa4b9d9d305e2549abe728d61
npx <vkodumuru@lyrostech.com>
2024-03-11 11:22:18 +0530

    commit

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <h1>Coffee Shop</h1>
+    <h1>Coffee Shop </h1>
     <Coffeeshop />
   </React.StrictMode>
 );
